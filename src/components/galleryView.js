b7dba8acb3ce8773b4e347474349cc4a9408b11c
popados <popados@gmail.com>
2026-04-11 21:15:10 -0500

    popa/images

--- src/components/galleryView.js
+++ src/components/galleryView.js
@@ -1,10 +1,10 @@
 const images = [
-    { src: "./img/cat-pictures/EAAF34D7-31D1-47B5-B01F-7F4860EBEF0F_1_105_c.jpeg", alt: "Cat 1" },
-    { src: "./img/cat-pictures/1243D6C6-D4DA-4AAE-ABE0-E6FC14D0C409_1_105_c.jpeg", alt: "Cat 2" },
-    { src: "./img/cat-pictures/B4F712CE-54A5-4221-AC3D-D432816D6D00_1_105_c.jpeg", alt: "Cat 3" },
-    { src: "./img/cat-pictures/19E6B226-4A54-49FC-81E7-8C56C3AC92B4_1_105_c.jpeg", alt: "Cat 4" },
-    { src: "./img/cat-pictures/955B83BF-D906-47E2-8E58-69A2C34B2674_1_105_c.jpeg", alt: "Cat 5" },
-    { src: "./img/cat-pictures/063447B0-D6D1-40B6-97CC-F8056CF8B3AA_1_105_c.jpeg", alt: "Cat 6" },
+    { src: "/img/cat-pictures/EAAF34D7-31D1-47B5-B01F-7F4860EBEF0F_1_105_c.jpeg", alt: "Cat 1" },
+    { src: "/img/cat-pictures/1243D6C6-D4DA-4AAE-ABE0-E6FC14D0C409_1_105_c.jpeg", alt: "Cat 2" },
+    { src: "/img/cat-pictures/B4F712CE-54A5-4221-AC3D-D432816D6D00_1_105_c.jpeg", alt: "Cat 3" },
+    { src: "/img/cat-pictures/19E6B226-4A54-49FC-81E7-8C56C3AC92B4_1_105_c.jpeg", alt: "Cat 4" },
+    { src: "/img/cat-pictures/955B83BF-D906-47E2-8E58-69A2C34B2674_1_105_c.jpeg", alt: "Cat 5" },
+    { src: "/img/cat-pictures/063447B0-D6D1-40B6-97CC-F8056CF8B3AA_1_105_c.jpeg", alt: "Cat 6" },
 ];
 
 export function createGalleryView() {
--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,16 @@
 import { resolve } from "node:path";
 import { fileURLToPath } from "node:url";
 import { defineConfig } from "vite";
+import { viteStaticCopy } from "vite-plugin-static-copy";
 
 const projectRoot = fileURLToPath(new URL(".", import.meta.url));
 
 export default defineConfig({
+    plugins: [
+        viteStaticCopy({
+            targets: [{ src: "img", dest: "" }]
+        })
+    ],
     server: {
         proxy: {
             "/api": "http://localhost:3000"
